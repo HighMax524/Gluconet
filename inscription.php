ea917155b2e466fdd4642d826231e754ec0f9dae
Maxime BOGNON <maximebognon@gmail.com>
2025-12-18 19:52:06 +0100

    ✨ Ajout du code pour l'inscription

--- inscription.php
+++ inscription.php
@@ -1,3 +1,51 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($host, $username, $password) or die("erreur de connexion");
+
+$namedb = "gluconet_db";
+$db = mysqli_select_db($conn, $namedb) or die("erreur de connexion base");
+
+mysqli_select_db($conn, $namedb) or die("erreur de connexion base");
+
+$table = "utilisateur";
+
+if (isset($_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['tel'], $_POST['mdp'], $_POST['conf_mdp'])) {
+    $clean_nom = strip_tags($_POST['nom']);
+    $clean_prenom = strip_tags($_POST['prenom']);
+    $clean_email = strip_tags($_POST['email']);
+    $clean_tel = strip_tags($_POST['tel']);
+    $clean_mdp = strip_tags($_POST['mdp']);
+    $clean_conf_mdp = strip_tags($_POST['conf_mdp']);
+
+    if ($clean_mdp !== $clean_conf_mdp) {
+        header('Location: inscription.php?error=Les mots de passe ne correspondent pas.');
+        exit();
+    }
+
+    $nom = $clean_nom;
+    $prenom = $clean_prenom;
+    $email = $clean_email;
+    $tel = $clean_tel;
+    $mdp = md5($clean_mdp);
+
+    $requete = "INSERT INTO $table (nom, prenom, email, tel, mdp) VALUES (?, ?, ?, ?, ?)";
+    $reqpre = mysqli_prepare($conn, $requete);
+    mysqli_stmt_bind_param($reqpre, "sssss", $nom, $prenom, $email, $tel, $mdp);
+
+    if (mysqli_stmt_execute($reqpre)) {
+        header('Location: role.php');
+        exit();
+    } else {
+        header('Location: index.php');
+        exit();
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -18,7 +66,7 @@
                 </div>
             <?php endif; ?>
             <h1 class="titre_form">S'inscrire</h1>
-            <form id="form_inscr" action="backend/traitement_inscription.php" method="post">
+            <form id="form_inscr" action="" method="post">
                 <input type="text" id="nom" name="nom" placeholder="Nom" required>
 
                 <input type="text" id="prenom" name="prenom" placeholder="Prénom" required>
@@ -32,6 +80,7 @@
                 <input type="password" id="conf_mdp" name="conf_mdp" placeholder="Confirmer mot de passe" required>
 
                 <input type="submit" value="S'inscrire" class="boutton_form">
+
             </form>
             <div class="deja_un_compte">
                 <p>Vous avez déja un compte ?</p>
