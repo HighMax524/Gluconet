e8c1895bcc16ca05a9168ad0a90626dab7bec58d
Maxime BOGNON <maximebognon@gmail.com>
2025-12-19 16:04:41 +0100

    ✨ modif page faq

--- faq.php
+++ faq.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="res/style.css">
     <title>FAQ - Gluconet</title>
 
     <style>
@@ -164,11 +165,8 @@
         </details>
 
     </section>
-
-    <footer>
-        💚 Gluconet – Garder le contrôle sur sa santé au quotidien
-    </footer>
 </div>
+<?php include 'footer.php'; ?>
 
 </body>
 </html>
